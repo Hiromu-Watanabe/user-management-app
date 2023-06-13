09d79db98734127bd77ac0155e4db4f094389a8c
渡邉　展夢 <86395724+Hiromu-Watanabe@users.noreply.github.com>
2023-06-13 14:55:25 +0000

    add: chakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Button, ChakraProvider } from "@chakra-ui/react";
 import "./styles.css";
 
 export default function App() {
+  // 2. Wrap ChakraProvider at the root of your app
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <ChakraProvider>
+      <Button colorScheme={"teal"}>ボタン</Button>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    </ChakraProvider>
   );
 }
