ab7326e6f8d267df70e92294da842ec2baa13ab3
渡邉　展夢 <86395724+Hiromu-Watanabe@users.noreply.github.com>
2023-06-15 17:37:18 +0000

    Update: "/home"は以下のルーティング設定

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,12 +2,31 @@ import { memo, FC } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import { Login } from "../components/pages/Login";
+import { homeRoutes } from "../router/HomeRoutes";
 
-//
 export const Router: FC = memo(() => {
   return (
     <Routes>
-      <Route path="/" element={<Login />}></Route>
+      <Route path="/" element={<Login />} />
+      <Route
+        path="home/*"
+        element={
+          <Routes>
+            {homeRoutes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+              />
+            ))}
+            {/* 下記コードは上記コードと同じことをやっている 
+            <Route index element={<Home />} />
+            <Route path="user_management" element={<UserManagement />} />
+            <Route path="setting" element={<Setting />} />
+            */}
+          </Routes>
+        }
+      />
     </Routes>
   );
 });
