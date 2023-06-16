fc7b153f2cfc6f7e8f84a2bd6cf385865a01058e
渡邉　展夢 <86395724+Hiromu-Watanabe@users.noreply.github.com>
2023-06-16 10:12:02 +0900

    update: 初期ページルーティング雛形 (#1)
    * add: chakraUI
    
    * add: 雛形wip
    
    * Update: React Routerでログイン画面表示
    
    * Update: "/home"は以下のルーティング設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+import theme from "./themes/themes";
 import "./styles.css";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
