2d4814be2aea442c37b8e5f8be514b875b27b73f
渡邉　展夢 <86395724+Hiromu-Watanabe@users.noreply.github.com>
2023-06-17 04:54:29 +0000

    wip: ヘッダー作成

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,8 +1,13 @@
 import { memo, FC } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import { Login } from "../components/pages/Login";
 import { homeRoutes } from "../router/HomeRoutes";
+import { Login } from "../components/pages/Login";
+// import { Home } from "../components/pages/Home";
+// import { UserManagement } from "../components/pages/UserManagement";
+// import { Setting } from "../components/pages/Setting";
+import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 
 export const Router: FC = memo(() => {
   return (
@@ -13,20 +18,19 @@ export const Router: FC = memo(() => {
         element={
           <Routes>
             {homeRoutes.map((route) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={route.element}
-              />
+              <Route key={route.path} path={route.path} element={route.element}>
+                <HeaderLayout>{route.element}</HeaderLayout>
+              </Route>
             ))}
-            {/* 下記コードは上記コードと同じことをやっている 
-            <Route index element={<Home />} />
+            {/* 下記コードは上記コードと同じことをやっている  */}
+            {/* <Route index element={<Home />} />
             <Route path="user_management" element={<UserManagement />} />
             <Route path="setting" element={<Setting />} />
-            */}
+            <Route path="*" element={<Page404 />} /> */}
           </Routes>
         }
       />
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 });
