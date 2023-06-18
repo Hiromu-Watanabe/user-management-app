007b9c6b9be825e6a8213426d666d96a462d9e57
渡邉　展夢 <86395724+Hiromu-Watanabe@users.noreply.github.com>
2023-06-19 02:24:49 +0900

    Draft/red shadow (#3)
    * add: chakraUI
    
    * add: 雛形wip
    
    * Update: React Routerでログイン画面表示
    
    * Update: "/home"は以下のルーティング設定
    
    * wip: ヘッダー作成
    
    * update: ESLint設定
    summary: PropTypesのチェックに関連しているもので、Reactコンポーネントに渡されるpropsの型や形状をチェックする設定無効
    
    * add: ChakraUIインストール
    
    * add: コミット漏れ(52eda9c153a7def43003a8ff098b15e9bfe619c0)
    
    * update: ヘッダー作成
    summary: ハンバーガーメニューとDrawerはatomsとmoleculesに分割
    
    * add: プライマリーボタン追加
    
    * update: Login.tsx
    summary: ログイン機能実装
    
    * update: useAuth.ts
    summary: ログイン時のメッセージ（トースト）実装
    
    * add: useAllUsers.ts
    summary: ユーザー一覧取得処理のカスタムフック作成
    
    * add: ユーザーカード
    
    * update: UserManagement.tsx
    summary: カスタムフックでユーザー情報取得,  取得情報でユーザーカード表示
    
    ---------
    
    Co-authored-by: hiromu watanabe <hiromu.watanabe0729@gmail.com>

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,37 @@
+import { useCallback, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import { user } from "../types/api/user";
+import { useMessage } from "./useMessage";
+
+export const useAuth = () => {
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+
+  // トーストのカスタムhook
+  const { showMessage } = useMessage();
+
+  const login = useCallback(
+    async (id: string) => {
+      try {
+        setLoading(true);
+
+        const res: user = (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data;
+        if (res) {
+          navigate("/home");
+          showMessage({ title: "ログインしました", status: "success" });
+        } else {
+          showMessage({ title: "ユーザーが見つかりません", status: "error" });
+        }
+      } catch (err) {
+        showMessage({ title: "ユーザーが見つかりません", status: "error" });
+      } finally {
+        setLoading(false);
+      }
+    },
+    [navigate, showMessage]
+  );
+  return { login, loading };
+};
