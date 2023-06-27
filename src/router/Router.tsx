66ffd20bfb57a0ec55ef91d94c60607ac3202f0d
Hiromu-Watanabe <hiro_mu0166@icloud.com>
2023-06-28 03:27:26 +0900

    コミット漏れ（0269f058fcd87624097a566aed819b349cf1ea72）

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,31 +5,38 @@ import { homeRoutes } from "../router/HomeRoutes";
 import { HeaderLayout } from "../components/templates/HeaderLayout";
 import { Page404 } from "../components/pages/Page404";
 import { Login } from "../components/pages/Login";
+import { LoginUserProvider } from "../providers/LoginUserProvider";
 // import { Home } from "../components/pages/Home";
 // import { UserManagement } from "../components/pages/UserManagement";
 // import { Setting } from "../components/pages/Setting";
 
 export const Router: FC = memo(() => {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route
-        path="home/*"
-        element={
-          <Routes>
-            {homeRoutes.map((route) => (
-              <Route key={route.path} path={route.path} element={<HeaderLayout>{route.element}</HeaderLayout>}></Route>
-            ))}
-            {/* 下記コードは上記コードと同じことをやっている  */}
-            {/* <Route index element={<Home />} />
+    <LoginUserProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="home/*"
+          element={
+            <Routes>
+              {homeRoutes.map((route) => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  element={<HeaderLayout>{route.element}</HeaderLayout>}
+                ></Route>
+              ))}
+              {/* 下記コードは上記コードと同じことをやっている  */}
+              {/* <Route index element={<Home />} />
             <Route path="user_management" element={<UserManagement />} />
             <Route path="setting" element={<Setting />} />
             <Route path="*" element={<Page404 />} /> */}
-          </Routes>
-        }
-      />
-      <Route path="*" element={<Page404 />} />
-    </Routes>
+            </Routes>
+          }
+        />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </LoginUserProvider>
   );
 });
 
